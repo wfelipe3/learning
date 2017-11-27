f96e7857c1158d965792c4c6810ab98f821ba323
Felipe Rojas <wfelipe3@gmail.com>
2017-11-27 13:20:58 -0500

    fib

--- clojure/clojure-basics/src/clojure_basics/core.clj
+++ clojure/clojure-basics/src/clojure_basics/core.clj
@@ -1,7 +1,18 @@
 (ns clojure-basics.core
   (:gen-class))
 
+(def add (fn [x y] (+ x y)))
+
+(defn fib
+  ([x] (do (println x)
+           (case x
+                0 0
+                1 1
+                (let [value (+ (fib (- x 1)) (fib (- x 2)))]
+                  (do (println (str "fib " value))
+                      value))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (fib 1)))
--- clojure/clojure-basics/test/clojure_basics/core_test.clj
+++ clojure/clojure-basics/test/clojure_basics/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [clojure-basics.core :refer :all]))
 
+(deftest fibonacci
+  (testing "several values"
+    (is (= 8 (fib 6)))))
+
+
 (deftest a-test
   (testing "first test"
     (is (= 1 1))))
