dce658533726693d3e3e294b27bed60b62148b69
Felipe Rojas <wfelipe3@gmail.com>
2017-12-16 18:38:36 -0500

    basics for strings, math, functions, if and lists

--- elm/basics/tests/Tests.elm
+++ elm/basics/tests/Tests.elm
@@ -13,12 +13,50 @@ all =
             [
                 test "first test" <|
                 \_ -> Expect.equal True True
+                , test "invoke test function" (\() -> Expect.equal True True)
             ]
-        , describe "Simple String"
+        , describe "String"
             [
                 test "Simple string " <|
                     \_ -> Expect.equal "Hello world" "Hello world"
                 , test "String concat" <|
                     \_ -> Expect.equal "hello world" ("hello " ++  "world")
             ]
+        , describe "math" 
+            [
+                test "math operations" <|
+                    \_ -> Expect.equal 14 (2 + 3 * 4)
+                , test "floating point division" <|
+                    \_ -> Expect.equal 4.5 (9 / 2)
+                , test "int division" <|
+                    \_ -> Expect.equal 4 (9//2)
+            ]
+        , describe "functions"    
+            [
+                test "function invokation" <| 
+                    \_ -> (isNegative 10) |> Expect.equal False
+            ]
+        , describe "if"
+            [
+                test "if expression" <|
+                    \_ -> (if True then "hello" else "world") |> Expect.equal "hello"
+                , test "if in functions" <| 
+                    \_ -> overNineThousand 10000 |> Expect.equal "It is over nine thousand"
+            ]
+        , describe "Lists"
+            [
+                test "create list" <|
+                    \_ -> ["Alice", "bob", "chuck"] |> Expect.equal ["Alice", "bob", "chuck"]
+                , test "is empty" <|
+                    \_ -> Expect.equal True (List.isEmpty [])
+                , test "list lenght" <| 
+                    \_ -> List.length [1, 2, 3] |> Expect.equal 3
+                        
+            ]
        ] 
+
+isNegative : Int -> Bool
+isNegative x = x < 0
+
+overNineThousand : Int -> String
+overNineThousand x = if x > 9000 then "It is over nine thousand" else "meeehh"
