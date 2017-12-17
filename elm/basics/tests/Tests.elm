eeb66c111e6ab822edfc38eaf16db9c00205a5f4
Felipe Rojas <wfelipe3@gmail.com>
2017-12-16 20:26:16 -0500

    elm basics for tuples, lists and records

--- elm/basics/tests/Tests.elm
+++ elm/basics/tests/Tests.elm
@@ -1,5 +1,6 @@
 module Tests exposing (..)
 
+import Tuple exposing (..)
 import Test exposing (..)
 import Expect
 
@@ -51,7 +52,62 @@ all =
                     \_ -> Expect.equal True (List.isEmpty [])
                 , test "list lenght" <| 
                     \_ -> List.length [1, 2, 3] |> Expect.equal 3
-                        
+                , test "reverse" <| 
+                    \_ -> List.reverse [1, 2, 3] |> Expect.equal [3, 2, 1]
+                , test "sort" <|
+                    \_ -> List.sort [6, 5, 4, 3, 2, 1] |> Expect.equal [1,2,3,4,5,6]
+                , test "map" <|
+                    \_ -> List.map (\i -> i * 2) [1, 2, 3, 4] |> Expect.equal [2, 4, 6, 8]
+                , test "map with pipe" <|
+                    \_ -> [1,2,3,4] |> List.map (\i -> i * 2) |> Expect.equal [2, 4, 6, 8]
+                , test "flatmap" <|
+                    \_ -> [1, 2, 3, 4] 
+                        |> List.concatMap (\i -> [1, i]) 
+                        |> Expect.equal [1, 1, 1, 2, 1, 3, 1, 4]
+            ]
+        , describe "tuples"
+            [
+                test "create tuple" <|
+                    \_ -> (True, "test") |> Expect.equal (True, "test")
+                , test "get elements from tuple" <|
+                    \_ -> Tuple.first (True, "hello") |> Expect.equal True
+                , test "pattern match on tuple" <|
+                    \_ -> 
+                        let 
+                            (first, _, _) =  (True, "felipe", "rojas") 
+                        in 
+                            first |> Expect.equal True 
+            ]
+        , describe "records"
+            [
+                test "creat a record" <|
+                    \_ -> {x = 1, y = 2} |> Expect.equal {x = 1, y = 2}
+                , test "get value from record" <|
+                    \_ ->
+                        let
+                            value = {x = 1, y = 2}
+                        in
+                           value.x |> Expect.equal 1 
+                , test "get value from record with function" <|
+                    \_ -> 
+                        let
+                            value = {x = 1, y = True}
+                        in
+                            .y value |> Expect.equal True
+                , test "map with tuple function" <|
+                    \_ ->
+                        let
+                            value = {x = 1, y = True}
+                        in
+                            List.map .y [value, value, value] |> Expect.equal [True, True, True]
+                , test "change value in record" <|
+                    \_ ->
+                        let
+                            value = {x = 3, y = 4}
+                            value2 = {value | y = 5}
+                        in
+                            .y value2 |> Expect.equal 5
+
             ]
        ] 
 
