bf648eb38999e6fa6a252f5226c118f015704969
Felipe Rojas <wfelipe3@gmail.com>
2018-01-27 22:49:18 -0500

    elm hello world

--- elm/basics/tests/Views.elm
+++ elm/basics/tests/Views.elm
@@ -10,7 +10,7 @@ suite =
         [ describe "some test"
             [ test "new unit test for this project"
                 (\_ -> Expect.equal 3 (2 + 1))
-            , test "other test" <|
-                \_ -> Expect.equal True True
+            , test "other test" <| 
+                \_ ->  Expect.equal True True
             ]
         ]
