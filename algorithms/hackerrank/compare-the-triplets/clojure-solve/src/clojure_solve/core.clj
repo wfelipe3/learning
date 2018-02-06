91125276566934d1eb663f46b993b13d54f2aceb
Felipe Rojas <wfelipe3@gmail.com>
2018-02-05 23:42:58 -0500

    compare the triplets clojure unsolved

---
+++ algorithms/hackerrank/compare-the-triplets/clojure-solve/src/clojure_solve/core.clj
@@ -0,0 +1,31 @@
+(ns clojure-solve.core
+  (:gen-class))
+
+(require '[clojure.string :as str])
+
+(defn get-values 
+  [] (str/split (read-line) #"\s+"))
+
+(defn int-values 
+  [values] (map #(Integer/parseInt %) values))
+
+(defn to-point 
+  [x y] (if (x > y)
+            [1 0]
+            (if (y > x)
+              [0 1]
+              [0 0])))
+
+(defn get-points 
+  [x y] (let [
+    range3 (take 3 (range)) 
+  ]
+  (map #(to-point (nth x %1) (nth y %1)) range3)))
+
+(defn final-score 
+  [p] (reduce #([(+ (nth %1 0)(nth %2 0)) (+ (nth %1 1)(nth %2 1))]) p))
+
+(defn -main
+  [& args]
+  (let [points (final-score (get-points (int-values (get-values)) (int-values (get-values))))]
+      (print (nth points 0)(nth points 1))))
