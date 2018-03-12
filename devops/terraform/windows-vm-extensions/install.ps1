0ebba6a7af911c5782119e9ce32b7d7d28dc5153
Felipe Rojas <wfelipe3@gmail.com>
2018-03-12 08:55:32 -0500

    chages

--- devops/terraform/windows-vm-extensions/install.ps1
+++ devops/terraform/windows-vm-extensions/install.ps1
@@ -1,3 +1,6 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install googlechrome -y
 choco install -y jdk8
+choco install -y visualstudiocode
+choco install -y packer
+choco install -y terraform
