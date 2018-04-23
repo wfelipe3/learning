f7e0bc867700a69621e8184fec5be4c8307ea3b3
wfelpe3 <wfelipe3@gmail.com>
2018-04-22 23:11:53 -0500

    compare triplets in f#

---
+++ algorithms/hackerrank/compare-the-triplets/fsharp/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let inputPoints =
+    let values = Console.ReadLine() 
+    values.Split ' ' |> Array.map int 
+
+[<EntryPoint>]
+let main argv =
+    printf "%A" inputPoints
+    printf "%A" inputPoints
+    0 // return an integer exit code
