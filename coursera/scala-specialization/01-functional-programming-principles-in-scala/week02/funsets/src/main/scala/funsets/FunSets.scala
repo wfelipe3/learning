99d7c861f7b0e78ca92be280e043e928f2b04317
Felipe Rojas <wfelipe3@gmail.com>
2018-08-20 16:23:37 -0500

    simplification of the anonymous function

--- coursera/scala-specialization/01-functional-programming-principles-in-scala/week02/funsets/src/main/scala/funsets/FunSets.scala
+++ coursera/scala-specialization/01-functional-programming-principles-in-scala/week02/funsets/src/main/scala/funsets/FunSets.scala
@@ -78,7 +78,7 @@ object FunSets {
     * Returns a set transformed by applying `f` to each element of `s`.
     */
   def map(s: Set, f: Int => Int): Set =
-    i => exists(s, x => f(x) == i)
+    i => exists(s, f(_) == i)
 
   /**
     * Displays the contents of a set
